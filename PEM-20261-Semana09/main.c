e37e1e97c4ff24988b219413ddc52b9372360e65
danieloliveiraa132006-droid <danieloliveiraa132006@gmail.com>
2026-05-28 18:58:30 -0300

    Add files via upload

---
+++ PEM-20261-Semana09/main.c
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------*
+*                      FATEC Ipiranga                         *
+*       Disciplina: Programação Estruturada e Modular         *
+*                   Prof. Carlos Veríssimo                    *
+*-------------------------------------------------------------*
+* Objetivo do Programa: Controlar o fluxo principal           *
+* do sistema e iniciar o menu da aplicação CUCC.              *
+* Data - 15/05/2026                                           *
+* Autor: Daniel Oliveira da Silva                                        *
+*-------------------------------------------------------------*/
+
+#include "interface.h"
+
+int main()
+{
+    menu();
+    return 0;
+}
+
