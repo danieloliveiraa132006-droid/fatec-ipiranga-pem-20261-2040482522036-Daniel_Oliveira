a0cae23a0d92a22f948dbbdcbfbcf5fa7ccd3c4a
danieloliveiraa132006-droid <danieloliveiraa132006@gmail.com>
2026-05-28 18:50:36 -0300

    Add files via upload

---
+++ PEM-20261-Semana09/interface.c.c
@@ -0,0 +1,70 @@
+/*------------------------------------------------------------*
+*                      FATEC Ipiranga                         *
+*       Disciplina: Programação Estruturada e Modular         *
+*                   Prof. Carlos Veríssimo                    *
+*-------------------------------------------------------------*
+* Objetivo do Programa: Implementar o menu interativo         *
+* e realizar a comunicação entre usuário e sistema.           *
+* Data - 15/05/2026                                           *
+* Autor: Daniel Oliveira da Silva                                         *
+*-------------------------------------------------------------*/
+
+#include <stdio.h>
+#include "interface.h"
+#include "recursao.h"
+
+void menu()
+{
+    int opcao;
+    int numero;
+    int base;
+    int expoente;
+
+    do
+    {
+        printf("\n===== CUCC =====\n");
+        printf("1 - Decimal para Binario\n");
+        printf("2 - Potencia\n");
+        printf("3 - Soma dos Digitos\n");
+        printf("0 - Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+            case 1:
+                printf("Digite um numero decimal: ");
+                scanf("%d", &numero);
+
+                printf("Binario: ");
+                decToBin(numero);
+                printf("\n");
+                break;
+
+            case 2:
+                printf("Digite a base: ");
+                scanf("%d", &base);
+
+                printf("Digite o expoente: ");
+                scanf("%d", &expoente);
+
+                printf("Resultado: %d\n", potencia(base, expoente));
+                break;
+
+            case 3:
+                printf("Digite um numero: ");
+                scanf("%d", &numero);
+
+                printf("Soma dos digitos: %d\n", somaDigitos(numero));
+                break;
+
+            case 0:
+                printf("Encerrando...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+        }
+
+    } while (opcao != 0);
+}
