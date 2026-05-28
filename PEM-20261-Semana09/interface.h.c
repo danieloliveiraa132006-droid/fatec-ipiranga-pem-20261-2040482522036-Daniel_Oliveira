2f3785360178786552237fdf8128419e0e3b2a7c
danieloliveiraa132006-droid <danieloliveiraa132006@gmail.com>
2026-05-28 18:54:04 -0300

    Add files via upload

---
+++ PEM-20261-Semana09/interface.h.c
@@ -0,0 +1,17 @@
+/*------------------------------------------------------------*
+*                      FATEC Ipiranga                         *
+*       Disciplina: Programação Estruturada e Modular         *
+*                   Prof. Carlos Veríssimo                    *
+*-------------------------------------------------------------*
+* Objetivo do Programa: Definir os protótipos das             *
+* funções responsáveis pela interface do usuário.             *
+* Data - 15/05/2026                                           *
+* Autor: Daniel Oliveira                                      *
+*-------------------------------------------------------------*/
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+void menu();
+
+#endif
