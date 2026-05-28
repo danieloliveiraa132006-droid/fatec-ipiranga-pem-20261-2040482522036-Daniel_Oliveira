16b3bd0a31954636d8102818d23f4d583e8dc3ed
danieloliveiraa132006-droid <danieloliveiraa132006@gmail.com>
2026-05-28 19:05:28 -0300

    Add files via upload

---
+++ PEM-20261-Semana09/recursao.h.c
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------*
+*                      FATEC Ipiranga                         *
+*       Disciplina: Programação Estruturada e Modular         *
+*                   Prof. Carlos Veríssimo                    *
+*-------------------------------------------------------------*
+* Objetivo do Programa: Declarar os protótipos das            *
+* funções recursivas utilizadas pelo sistema.                 *
+* Data - 15/05/2026                                           *
+* Autor: Daniel Oliveira da Silva                             *
+*-------------------------------------------------------------*/
+
+#ifndef RECURSAO_H
+#define RECURSAO_H
+
+void decToBin(int n);
+int potencia(int base, int exp);
+int somaDigitos(int n);
+
+#endif
